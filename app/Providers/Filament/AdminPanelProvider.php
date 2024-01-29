a9931fd7c3706719d27aa7c08a63357fc5dfd03c
I Wayan Rupadana <rupadanawayan@gmail.com>
2024-01-29 23:44:38 +0800

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Rupadana\FilamentUserResource\FilamentUserResourcePlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -32,6 +33,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->plugins([
+                FilamentUserResourcePlugin::make()
+            ])
             ->pages([
                 Pages\Dashboard::class,
             ])
