3583ef1757e3e428ba8bc2f09e2e178711ebc137
I Wayan Rupadana <rupadanawayan@gmail.com>
2024-02-04 04:07:08 +0800

    Refactor user authentication logic

--- routes/web.php
+++ routes/web.php
@@ -23,39 +23,6 @@ use Rupadana\GithubApi\GithubApi;
 
 Route::group(['middleware' => ['web']], function () {
     Route::get('/', function () {
-
-        $user = Socialite::driver('github')->userFromToken(ConnectedAccount::query()->where('user_id', auth()->user()->id)->first()->token);
-        
-
-        $github = GithubApi::make(ConnectedAccount::query()->where('user_id', auth()->user()->id)->first()->token);
-
-        dd($github->commits('app.codecrafters.id')->first());
-
-
-        $name = $user->getNickname();
-        $token = $user->token;
-        // We generate the url for curl
-        $curl_url = 'https://api.github.com/repos/' . $name . '/app.codecrafters.id/commits';
-
-        // We generate the header part for the token
-        $curl_token_auth = 'Authorization: token ' . $token;
-
-        // We make the actuall curl initialization
-        $ch = curl_init($curl_url);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        // We set the right headers: any user agent type, and then the custom token header part that we generated
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent: Awesome-Octocat-App', $curl_token_auth));
-
-        // We execute the curl
-        $output = curl_exec($ch);
-
-        // And we make sure we close the curl       
-        curl_close($ch);
-
-        // Then we decode the output and we could do whatever we want with it
-        $output = json_decode($output);
-        dd($output);
+        return "hello world";
     });
 });
